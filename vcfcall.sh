953bd7937554a9fe95f3ec464a2256b901b5655b
Jialin Ma <marlin-@gmx.cn>
2018-11-28 01:48:56 -0500

    Increase the number of threads to use

--- vcfcall.sh
+++ vcfcall.sh
@@ -5,17 +5,23 @@ which bcftools
 which tabix
 
 # Clean up, sort and index with samtools
-samtools fixmate -O bam bwamem.sam bwamemfixmate.bam
+echo ---- Running samtools fixmate ----
+samtools fixmate -@ 6 -O bam bwamem.sam bwamemfixmate.bam
 
-samtools sort -O bam -o sorted.bam -T ./sorttemp bwamemfixmate.bam
+echo ---- Running samtools sort ----
+samtools sort -@ 6 -m 1400M -O bam -o sorted.bam -T ./sorttemp bwamemfixmate.bam
 
-samtools index sorted.bam
+echo ---- Running samtools index ----
+samtools index -@ 6 sorted.bam
 
 # Call variants with mpileup
 
-samtools mpileup -ugf data/GRCh38_full_analysis_set_plus_decoy_hla.fa sorted.bam | \
-  bcftools call -vmO z -o HG00698_ERR031924.vcf.gz
+echo ---- Runnin samtools mpileup and bcftools call ----
+samtools mpileup -@ 6 -ugf data/GRCh38_full_analysis_set_plus_decoy_hla.fa sorted.bam | \
+  bcftools call --threads 6 -vmO z -o HG00698_ERR031924.vcf.gz
 
 # Index
+
+echo ---- Running tabix ----
 tabix -p vcf HG00698_ERR031924.vcf.gz
 
